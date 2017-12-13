3272f526f26b92322ba6c678c90bdb0692126aa0
derekwang1994 <derekwang1994@yahoo.com>
2017-12-12 20:34:17 -0800

    Cassandra connected successfully

--- SparkKafkaStreaming/build.sbt
+++ SparkKafkaStreaming/build.sbt
@@ -13,19 +13,12 @@ mainClass in (Compile, run) := Some("com.owen.sparkstreaming.AverageTweetLength"
 
 mainClass in (Compile, packageBin) := Some("com.owen.sparkstreaming.AverageTweetLength")
 
-libraryDependencies ++= {
-  val sparkVer = "1.5.2"
-  val twitterVer = "4.0.4"
-  val twitterStreamVer = "1.6.3"
-  Seq(
-//    "org.apache.spark" %% "spark-core" % sparkVer % "provided" withSources(),
-//    "org.apache.spark" % "spark-streaming_2.11" % sparkVer % "provided" withSources(),
-    "org.apache.spark" %% "spark-core" % sparkVer,
-    "org.apache.spark" % "spark-streaming_2.11" % sparkVer,
-    "org.apache.spark" % "spark-streaming-kafka_2.11" % sparkVer,
-    "org.twitter4j" % "twitter4j-core" % twitterVer,
-    "org.twitter4j" % "twitter4j-stream" % twitterVer,
-    "org.apache.spark" %% "spark-streaming-twitter" % twitterStreamVer
-  )
-}
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.0.0"
+libraryDependencies += "org.apache.spark" %% "spark-streaming" % "2.0.0"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.0.0"
+libraryDependencies += "org.apache.spark" %% "spark-streaming-kafka" % "1.6.0"
+libraryDependencies += "com.datastax.spark" %% "spark-cassandra-connector" % "2.0.0-M3"
+libraryDependencies += "org.twitter4j" % "twitter4j-core" % "4.0.4"
+libraryDependencies += "org.twitter4j" % "twitter4j-stream" % "4.0.4"
+libraryDependencies += "org.apache.spark" %% "spark-streaming-twitter" % "1.6.3"
 
--- SparkKafkaStreaming/src/main/scala/com/owen/sparkstreaming/KafkaToSpark.scala
+++ SparkKafkaStreaming/src/main/scala/com/owen/sparkstreaming/KafkaToSpark.scala
@@ -1,14 +1,14 @@
 package com.owen.sparkstreaming
 
 import Utilities._
-import org.apache.spark.SparkConf
+import org.apache.spark._
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.storage.StorageLevel
-import java.util.regex.Pattern
-import java.util.regex.Matcher
 
 import kafka.serializer.StringDecoder
 import org.apache.spark.streaming.kafka._
+import com.datastax.spark.connector._
+
 
 object KafkaToSpark {
 
@@ -17,44 +17,37 @@ object KafkaToSpark {
   def main(args: Array[String]) {
 
     // Create the context with a 1 second batch size
-    val ssc = new StreamingContext("local[*]", "KafkaExample", Seconds(1))
-
+    val sc = new SparkConf(true).set("spark.cassandra.connection.host", "127.0.0.1").setAppName("StreamingKafkaConsumer").setMaster("local[*]")
+    val ssc = new StreamingContext(sc, Seconds(2))
     setupLogging()
 
-    // Construct a regular expression (regex) to extract fields from raw Apache log lines
-    val pattern = apacheLogPattern()
 
     // hostname:port for Kafka brokers, not Zookeeper
-    val kafkaParams = Map("metadata.broker.list" -> "localhost:9092")
+    val kafkaParams = Map("metadata.broker.list" -> "localhost:9092", "auto.offset.reset" -> "smallest")
     // List of topics you want to listen for from Kafka
     val topics = List("twitter").toSet
     // Create our Kafka stream, which will contain (topic,message) pairs. We tack a
-    // map(_._2) at the end in order to only get the messages, which contain individual
-    // lines of data.
-    val lines = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topics).map(_._2)
+    // map(_._2) at the end in order to only get the messages, which contain individual lines of data.
+    val lines = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topics)
 
     lines.foreachRDD((rdd, time) => {
       rdd.collect().foreach(println)
     })
 
 
-//      // Extract the request field from each log line
-//      val requests = lines.map(x => {val matcher:Matcher = pattern.matcher(x); if (matcher.matches()) matcher.group(5)})
-//
-//      // Extract the URL from the request
-//      val urls = requests.map(x => {val arr = x.toString().split(" "); if (arr.size == 3) arr(1) else "[error]"})
-//
-//      // Reduce by URL over a 5-minute window sliding every second
-//      val urlCounts = urls.map(x => (x, 1)).reduceByKeyAndWindow(_ + _, _ - _, Seconds(300), Seconds(1))
-//
-//      // Sort and print the results
-//      val sortedResults = urlCounts.transform(rdd => rdd.sortBy(x => x._2, false))
-//      sortedResults.print()
+    lines.foreachRDD((rdd, time) => {
+      rdd.cache()
+      println("Writing "+rdd.count()+" rows to Cassandra")
+      rdd.saveToCassandra("test", "twitter", SomeColumns("id", "tweet"))
+    })
+
+
 
     // Kick it off
     ssc.checkpoint("./checkpoint/")
     ssc.start()
     ssc.awaitTermination()
+
   }
 
 }
